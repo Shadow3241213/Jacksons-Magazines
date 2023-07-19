5073a61806013d65acc4352dee08930df52984d2
Shadow3241213 <nikitakramsaninov@yahoo.com>
2023-07-19 22:02:56 +0100

    Initial Upload
    Uploading the Mod to github

---
+++ mod.cpp
@@ -0,0 +1,17 @@
+name = "Jackson's Mixed Magazines";
+picture = "logo_co.paa";
+logoSmall = "Logo_small_co.paa";
+logo = "logo_co.paa";
+logoOver = "logo_co.paa";
+tooltipOwned = "Jackson's Magazines";
+dlcColor[] =
+{
+	0.23,
+	0.39,
+	0.30,
+	1
+};
+overview = "Adds Mixed magazines for 3CB and RHS weapons";
+hideName = 0;
+hidePicture = 0;
+author = "J. Jackson";
